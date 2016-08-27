8e2738388d4f67f0feed5d4ff8f6ad12bf4e24df
yiooxir <spectrs@bk.ru>
2016-08-27 20:29:06 +0300

    modify eslint settings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-
 import './App.scss';
 
 export default class App extends Component {
   render() {
-
     return (
       <div className="App">
         Hello world ...
